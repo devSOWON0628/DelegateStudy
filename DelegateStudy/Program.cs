46b109ad738e915217f450b6a0c22586d2de4183
소원 박 <s2018s29@e-mirim.hs.kr>
2020-10-10 21:38:55 +0900

    delegate 예제

--- DelegateStudy/Program.cs
+++ DelegateStudy/Program.cs
@@ -6,6 +6,48 @@ using System.Threading.Tasks;
 
 namespace DelegateStudy
 {
+    class Student
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name+":" + this.Score;
+        }
+
+        public Student(string name, double score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+
+    }
+
+    class Students
+    {
+        private List<Student> listOfStudent = new List<Student>();
+        public delegate void PrintProcess(Student list);
+        public void Add(Student student)
+        {
+            listOfStudent.Add(student);
+        }
+        public void Print()
+        {
+            Print((student) =>
+            {
+                Console.WriteLine(student);
+            });
+        }
+
+        public void Print(PrintProcess process)
+        {
+            foreach (var item in listOfStudent)
+            {
+                process(item);
+            }
+        }
+    }
     //delegate class 내부,외부 에서 선언할 수 있음
     class Program
     {
@@ -26,6 +68,20 @@ namespace DelegateStudy
             delegateA();
             delegateB();
             delegateC();
+
+            ////////////////////////
+
+            Students students = new Students();
+            students.Add(new Student("박소원", 4.2));
+            students.Add(new Student("김정우", 3.3));
+
+            students.Print();
+            students.Print((student) =>
+            {
+                Console.WriteLine();
+                Console.WriteLine("이름: " + student.Name);
+                Console.WriteLine("점수: " + student.Score);
+            });
         }
         static void TestMethod()
         {
