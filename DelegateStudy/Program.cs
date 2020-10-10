f90cd21a7bc5317112635d32850df3175936ac2e
소원 박 <s2018s29@e-mirim.hs.kr>
2020-10-10 21:18:55 +0900

    delegate

--- DelegateStudy/Program.cs
+++ DelegateStudy/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace DelegateStudy
 {
+    //delegate class 내부,외부 에서 선언할 수 있음
     class Program
     {
+        public delegate void TestDelegate();
+
+        public delegate void CustomDelegate();
+        public void Method(CustomDelegate customDelegate)
+        {
+            customDelegate();
+        }
+
         static void Main(string[] args)
         {
+            TestDelegate delegateA=TestMethod;
+            TestDelegate delegateB=delegate() { };
+            TestDelegate delegateC = () => { };
+
+            delegateA();
+            delegateB();
+            delegateC();
+        }
+        static void TestMethod()
+        {
+
         }
     }
 }
